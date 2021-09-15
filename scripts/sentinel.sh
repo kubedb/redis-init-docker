565b3c33358a57e6adcc26507b62a072b41a6fce
Emon46 <emon@appscode.com>
2021-09-15 13:15:39 +0600

    fix: All Redis Sentinel crush recovery fix: Update Log for script fix: Add requirepass inside conf file Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/sentinel.sh
+++ scripts/sentinel.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-
+function timestamp() {
+    date +"%Y/%m/%d %T"
+}
+function log() {
+    local type="$1"
+    local msg="$2"
+    echo "$(timestamp) [$type] $msg"
+}
 function waitForPong() {
-    for j in {120..0}; do
+    log "INFO" "Trying to PING $HOSTNAME.$GOVERNING_SERVICE"
+    while true; do
         if [[ "${TLS:-0}" == "ON" ]]; then
             out=$(timeout 3 redis-cli -h "$HOSTNAME.$GOVERNING_SERVICE" -p 26379 --tls --cert /certs/client.crt --key /certs/client.key --cacert /certs/ca.crt ping)
         else
             out=$(timeout 3 redis-cli -h "$HOSTNAME.$GOVERNING_SERVICE" -p 26379 ping)
         fi
-        echo "Trying to ping: Step='$j', '$HOSTNAME'.'$GOVERNING_SERVICE' ,  Got='$out'"
         if [[ "$out" == "PONG" ]]; then
             break
         fi
         echo -n .
         sleep 1
     done
+    log "INFO" "Succefully Get PONG from $HOSTNAME.$GOVERNING_SERVICE"
 }
 
 function resetSentinel() {
-    echo "reset Sentinel $HOSTNAME "
+    log "INFO"  "resetting Sentinel $HOSTNAME..."
     waitForPong
     if [[ "${TLS:-0}" == "ON" ]]; then
         timeout 3 redis-cli -h "$HOSTNAME.$GOVERNING_SERVICE" -p 26379 --tls --cert /certs/client.crt --key /certs/client.key --cacert /certs/ca.crt SENTINEL RESET "*"
@@ -28,14 +36,15 @@ function resetSentinel() {
 
 function setSentinelConf() {
     echo "sentinel announce-ip $HOSTNAME.$GOVERNING_SERVICE" >>/data/sentinel.conf
+    echo "requirepass $REDISCLI_AUTH" >>/data/sentinel.conf
+    echo "masterauth $REDISCLI_AUTH" >>/data/sentinel.conf
 }
 
 flag=1
 not_exists_dns_entry() {
     myip=$(hostname -i)
-    echo " my ip $myip"
     if [[ -z "$(getent ahosts "$GOVERNING_SERVICE" | grep "^${myip}")" ]]; then
-        echo "$GOVERNING_SERVICE does not contain the IP of this pod: ${myip}"
+        log "WARNING" "$GOVERNING_SERVICE does not contain the IP of this pod: ${myip}"
         flag=1
     else
         echo "$GOVERNING_SERVICE has my IP: ${myip}"
@@ -44,14 +53,13 @@ not_exists_dns_entry() {
 }
 
 while [[ flag -ne 0 ]]; do
-    echo "flag =  $flag "
     not_exists_dns_entry
     sleep 1
 done
 args=$@
 # if /data/sentinel.conf file not available
 if [[ ! -f /data/sentinel.conf ]]; then
-    cp /conf/sentinel.conf /data/sentinel.conf
+    cp /scripts/sentinel.conf /data/sentinel.conf
     setSentinelConf
     exec redis-sentinel /data/sentinel.conf $args
 else
