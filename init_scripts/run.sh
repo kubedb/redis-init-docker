e10ba114ba178c2d184f4a2c51f0ab8993bc4af6
Emon46 <emon@appscode.com>
2022-04-25 14:58:47 +0600

    Fix: Use Different Directory For different Redis Types Fix: Auth issue Fix
    Signed-off-by: Emon46 <emon@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-cp /tmp/scripts/* /scripts
+if [ "$REDIS_TYPE" = "Cluster"]; then
+  cp /tmp/scripts/cluster/$MAJOR_REDIS_VERSION/* /scripts
+elif [ "$REDIS_TYPE" = "Sentinel" ]; then
+  cp /tmp/scripts/sentinel/* /scripts
+fi
