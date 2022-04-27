56b2f63001ca5165797b427047626a9f77df183f
Shaad7 <abdullah.alshaad@appscode.com>
2022-04-27 11:02:40 +0600

    redis cluster fix - dynamic failover
    Signed-off-by: Shaad7 <abdullah.alshaad@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ "$REDIS_TYPE" = "Cluster"]; then
-  cp /tmp/scripts/cluster/$MAJOR_REDIS_VERSION/* /scripts
+if [ "$REDIS_TYPE" = "Cluster" ]; then
+    cp /tmp/scripts/cluster/$MAJOR_REDIS_VERSION/* /scripts
+    cp /tmp/scripts/redis-node-finder /scripts/
 elif [ "$REDIS_TYPE" = "Sentinel" ]; then
-  cp /tmp/scripts/sentinel/* /scripts
+    cp /tmp/scripts/sentinel/* /scripts
 fi
