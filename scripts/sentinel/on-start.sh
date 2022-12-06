b7eab2f48bc2d4eff676aece83d665ad82a1928f
Shaad7 <abdullah.alshaad@appscode.com>
2022-12-06 12:10:42 +0600

    Fix Redis Auth Issue
    Signed-off-by: Shaad7 <abdullah.alshaad@appscode.com>

--- scripts/sentinel/on-start.sh
+++ scripts/sentinel/on-start.sh
@@ -12,9 +12,11 @@ function log() {
 
 function setUpSentinelArgs() {
   if [[ "${SENTINEL_PASSWORD:-0}" != 0 ]]; then
-      auth_args=("${auth_args[@]} -a ${SENTINEL_PASSWORD} --no-auth-warning")
+      log "Sentinel_Args" "Setting up Sentinel auth args"
+      sentinel_auth_args=("${sentinel_auth_args[@]} -a ${SENTINEL_PASSWORD} --no-auth-warning")
   fi
   if [[ "${SENTINEL_TLS:-0}" == "ON" ]]; then
+      log "Sentinel_Args" "Setting up Sentinel TLS Args"
       ca_crt=/certs/ca.crt
       client_cert=/certs/client.crt
       client_key=/certs/client.key
@@ -24,11 +26,15 @@ function setUpSentinelArgs() {
       fi
       sentinel_tls_args=("--tls --cert ${client_cert} --key ${client_key} --cacert ${ca_crt}")
   fi
-  sentinel_args=("${auth_args[@]} ${sentinel_tls_args[@]}")
-  log "Sentinel_Args" "${sentinel_args[*]}"
+  sentinel_args=("${sentinel_auth_args[@]} ${sentinel_tls_args[@]}")
 }
-function setUpRedisTLSArgs() {
+function setUpRedisArgs() {
+   if [[ "${REDISCLI_AUTH:-0}" != 0 ]]; then
+        log "Redis_Args" "Setting up Redis auth args"
+        redis_auth_args=("${redis_auth_args[@]} -a ${REDISCLI_AUTH} --no-auth-warning")
+    fi
    if [[ "${TLS:-0}" == "ON" ]]; then
+        log "Redis_Args" "Setting Up Redis TLS Args"
         ca_crt=/certs/ca.crt
         client_cert=/certs/client.crt
         client_key=/certs/client.key
@@ -36,14 +42,14 @@ function setUpRedisTLSArgs() {
             log "TLs is enabled, but $ca_crt, $client_cert or $client_key file does not exists "
             exit 1
         fi
-        tls_args=("--tls --cert ${client_cert} --key ${client_key} --cacert ${ca_crt}")
+        redis_tls_args=("--tls --cert ${client_cert} --key ${client_key} --cacert ${ca_crt}")
     fi
-    log "Redis_Args" "${tls_args[*]}"
+    redis_args=("${redis_auth_args[@]} ${redis_tls_args[@]}")
 }
 
 function setUpInitialThings() {
     setUpSentinelArgs
-    setUpRedisTLSArgs
+    setUpRedisArgs
     down=5000
     timeout=5000
 
@@ -59,7 +65,7 @@ function setUpInitialThings() {
 }
 
 function waitForSentinelToBeReady() {
-    log "INFO" "wait for $1.$2 sentinel to be ready!"
+    log "INFO" "Wait for $1.$2 sentinel to be ready!"
     while true; do
         timeout 3 redis-cli -h "$1.$2" -p 26379 ${sentinel_args[@]} ping &>/dev/null && break
         sleep 1
@@ -67,23 +73,29 @@ function waitForSentinelToBeReady() {
 }
 
 function waitForRedisToBeReady() {
-    log "INFO" "Attempting query on $1"
+    log "INFO" "Wait for $1 redis to be ready"
     while true; do
-        timeout 3 redis-cli -h "$1" -p 6379 ${tls_args[@]} ping 2>/dev/null && break
+        timeout 3 redis-cli -h "$1" -p 6379 ${redis_args[@]} ping &>/dev/null && break
         sleep 2
     done
 }
 
 function resetSentinel() {
     waitForSentinelToBeReady "$1" "$SENTINEL_GOVERNING_SERVICE"
-    log "INFO" "resetting sentinel $1.$SENTINEL_GOVERNING_SERVICE"
-    redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel reset "$REDIS_CLUSTER_REGISTERED_NAME" 2>/dev/null
+    log "INFO" "Resetting Sentinel $1.$SENTINEL_GOVERNING_SERVICE"
+    redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel reset "$REDIS_CLUSTER_REGISTERED_NAME" &>/dev/null
 }
 
 function removeClusterFromSpecificSentinel() {
     waitForSentinelToBeReady "$1" "$SENTINEL_GOVERNING_SERVICE"
-    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL REMOVE "$REDIS_CLUSTER_REGISTERED_NAME"
+    RESP=$(timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL REMOVE "$REDIS_CLUSTER_REGISTERED_NAME")
 
+    log "SENTINEL" "Remove Master from $SENTINEL_NAME-$i : $RESP"
+}
+function removeMasterGroupFromAllSentinel() {
+    for ((i = 0; i < $sentinel_replica_count; i++)); do
+        removeClusterFromSpecificSentinel "$SENTINEL_NAME-$i"
+    done
 }
 
 function findSDownStateForAllReplicasInfo() {
@@ -125,7 +137,7 @@ function findSDownStateForAllReplicasInfo() {
 }
 
 function waitToSyncSentinelConfig() {
-    log "INFO" "checking if all sentinel's configuration are up-to-date..."
+    log "INFO" "Checking if all sentinel's configuration are up-to-date..."
     for ((j = 0; j < $sentinel_replica_count; j++)); do
         REPLICAS_INFO_FROM_SENTINEL=$(redis-cli -h "$SENTINEL_NAME-$j".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel replicas "$REDIS_CLUSTER_REGISTERED_NAME" 2>/dev/null)
         if [[ "${#REPLICAS_INFO_FROM_SENTINEL}" == "0" ]]; then
@@ -140,43 +152,35 @@ function waitToSyncSentinelConfig() {
         fi
         sleep 1
     done
-    log "INFO" "all sentinel's configuration are up-to-date!!"
+    log "INFO" "All sentinel's configuration are up-to-date!!"
 
-}
-
-function removeMasterGroupFromAllSentinel() {
-    for ((i = 0; i < $sentinel_replica_count; i++)); do
-        waitForSentinelToBeReady "$SENTINEL_NAME-$i" "$SENTINEL_GOVERNING_SERVICE"
-        timeout 3 redis-cli -h $SENTINEL_NAME-"$i".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL REMOVE "$REDIS_CLUSTER_REGISTERED_NAME"
-    done
-}
-# Here we get the sentnels and tell each sentinel which master to monitor which is currently created redis
-# We also set some configuration here like auth pass (if any) , failover timeout and down-after-milisecond
-# This function is called only for master and only when the redis object is first initiating
-function addConfigurationWithAllSentinel() {
-    for ((i = 0; i < $sentinel_replica_count; i++)); do
-        waitForSentinelToBeReady "$SENTINEL_NAME-$i" "$SENTINEL_GOVERNING_SERVICE"
-        timeout 3 redis-cli -h $SENTINEL_NAME-"$i".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL MONITOR $REDIS_CLUSTER_REGISTERED_NAME "$1" 6379 $sentinel_quorum_val
-        if [[ "${REDISCLI_AUTH:-0}" != 0 ]]; then
-            timeout 3 redis-cli -h $SENTINEL_NAME-"$i".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL SET $REDIS_CLUSTER_REGISTERED_NAME auth-pass "$REDISCLI_AUTH"
-        fi
-        timeout 3 redis-cli -h $SENTINEL_NAME-"$i".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME failover-timeout $timeout
-        timeout 3 redis-cli -h $SENTINEL_NAME-"$i".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME down-after-milliseconds $down
-    done
 }
 
 function addConfigurationWithSpecificSentinel() {
     waitForSentinelToBeReady "$1" "$SENTINEL_GOVERNING_SERVICE"
-    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL MONITOR $REDIS_CLUSTER_REGISTERED_NAME "$2" 6379 $sentinel_quorum_val
+    log "SENTINEL" "Setting up Configuration for $1"
+    RESP=$(timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL MONITOR $REDIS_CLUSTER_REGISTERED_NAME "$2" 6379 $sentinel_quorum_val 2>/dev/null)
+    log "CONF" "Monitor Master: $RESP"
     if [[ "${REDISCLI_AUTH:-0}" != 0 ]]; then
-        timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL SET $REDIS_CLUSTER_REGISTERED_NAME auth-pass "$REDISCLI_AUTH"
+        timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL SET $REDIS_CLUSTER_REGISTERED_NAME auth-pass "$REDISCLI_AUTH" &>/dev/null
     fi
-    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME failover-timeout $timeout
-    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME down-after-milliseconds $down
+    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME failover-timeout $timeout &>/dev/null
+    timeout 3 redis-cli -h "$1".$SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} sentinel set $REDIS_CLUSTER_REGISTERED_NAME down-after-milliseconds $down &>/dev/null
 }
 
+# Here we get the sentinels and tell each sentinel which master to monitor which is currently created redis
+# We also set some configuration here like auth pass (if any) , failover timeout and down-after-milisecond
+# This function is called only for master and only when the redis object is first initiating
+function addConfigurationWithAllSentinel() {
+    REDIS_MASTER_DNS=$1
+    for ((i = 0; i < $sentinel_replica_count; i++)); do
+        addConfigurationWithSpecificSentinel "$SENTINEL_NAME-$i" $REDIS_MASTER_DNS
+    done
+}
+
+
 function getMasterHost() {
-    log "INFO" "trying to get master host"
+    log "INFO" "Trying to get master host"
     sentinel_info_command=$(timeout 3 redis-cli -h $SENTINEL_GOVERNING_SERVICE -p 26379 ${sentinel_args[@]} SENTINEL get-master-addr-by-name $REDIS_CLUSTER_REGISTERED_NAME 2>/dev/null)
     REDIS_SENTINEL_INFO=()
     for line in $sentinel_info_command; do
@@ -192,7 +196,7 @@ function getMasterHost() {
 #Check if all sentinel is ready or not
 #If not ready then make sure 0th redis instance as master & other redis instance as replica of 0th pod
 function isReadyAllSentinel() {
-    log "INFO" "check if all sentinels are ready"
+    log "INFO" "Check if all sentinels are ready"
     #if the number of pong == replica of sentinel , that means all sentinel is ready.
     for ((i = 0; i < $sentinel_replica_count; i++)); do
         waitForSentinelToBeReady "$SENTINEL_NAME-$i" "$SENTINEL_GOVERNING_SERVICE"
@@ -249,7 +253,7 @@ not_exists_dns_entry() {
     # Gets IP of host pod
     myip=$(hostname -i)
 
-    log "INFO" "check if $REDIS_GOVERNING_SERVICE contains the IP of this pod: ${myip}"
+    log "INFO" "Check if $REDIS_GOVERNING_SERVICE contains the IP of this pod: ${myip}"
     if [[ -z "$(getent ahosts "$REDIS_GOVERNING_SERVICE" | grep "^${myip}")" ]]; then
         flag=1
     else
@@ -271,7 +275,7 @@ isReadyAllSentinel
 getMasterHost
 args=$@
 if [[ "${#REDIS_SENTINEL_INFO[@]}" == "0" ]]; then
-    log "INFO" "initializing the redis server for the first time..."
+    log "INFO" "Initializing Redis server for the first time..."
     self="$HOSTNAME.$REDIS_GOVERNING_SERVICE"
     if [[ $HOSTNAME == "$REDIS_NAME-0" ]]; then
         exec redis-server /data/default.conf $args &
@@ -295,10 +299,10 @@ if [[ "${#REDIS_SENTINEL_INFO[@]}" == "0" ]]; then
         pid=$!
     fi
 else
-    log "INFO" "Got master info from sentinel . Pod restarting maybe ?"
+    log "INFO" "Got master info from sentinel"
     self="$HOSTNAME.$REDIS_GOVERNING_SERVICE"
     if [ "$self" != "${REDIS_MASTER_DNS:-0}" ]; then
-        log "INFO" "checking if $REDIS_MASTER_DNS ready as primary!"
+        log "INFO" "Checking if $REDIS_MASTER_DNS ready as primary!"
         waitForRedisToBeReady $REDIS_MASTER_DNS
         echo "replicaof $REDIS_MASTER_DNS $REDIS_MASTER_PORT_NUMBER" >>/data/default.conf
     fi
@@ -309,7 +313,7 @@ else
         for ((i = 0; i < $sentinel_replica_count; i++)); do
             findMasterState "$SENTINEL_NAME-$i"
             if [ "$s_down" == "true" ]; then
-                log "WARNING" "need to remove the cluster and add again as master is in s_down state"
+                log "WARNING" "Need to remove the cluster and add again as master is in s_down state"
                 removeClusterFromSpecificSentinel "$SENTINEL_NAME-$i"
                 addConfigurationWithSpecificSentinel "$SENTINEL_NAME-$i" "$REDIS_MASTER_DNS"
             fi
